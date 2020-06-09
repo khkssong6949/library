ca750e63e3066ee5f517b5211feaf78e0ab6c70a
khkssong6949 <bada6949@gmail.com>
2020-06-09 21:14:28 +0900

    glide 사용

--- app/src/main/java/kr/co/hgney/library/MainActivity.kt
+++ app/src/main/java/kr/co/hgney/library/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.hgney.library
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -23,7 +24,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        Glide.with(mContext).load("https://s.marketwatch.com/public/resources/images/MW-HT282_dollar_ZG_20191014195738.jpg").into(profileImg)
     }
 
 }
